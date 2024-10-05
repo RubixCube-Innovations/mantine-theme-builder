49a9749e0f5a27ba0e2d4b69b5732c0d0fcd01b1
Gopu K Raju <gopukraju02@gmail.com>
2024-10-05 15:41:10 +0530

    feat: dark mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { Container, Text, Title, Box, Stack } from '@mantine/core'
-import MantineCards from './components/cards/mantine-cards'
+import { Container, Text, Title, Box, Stack, Flex } from "@mantine/core";
+import MantineCards from "./components/cards/mantine-cards";
+import ColorSchemeSwitch from "./components/color-scheme-switch/color-scheme-switch";
 
 export default function ThemesPage() {
   return (
     <Container size="xl" px="md" py={"xl"} h={"100%"}>
       <Stack align="flex-start" h={"100%"}>
-        <Box>
-          <Title order={1}>
-            Mantine Theme Builder
-          </Title>
-          <Text>
-            Hand-picked shadcn inspired themes that you can copy and paste into your apps build with Mantine.
-          </Text>
-          <Box>
-            {/* ThemeCustomizer can be ignored */}
-          </Box>
-        </Box>
-       <MantineCards/>
+        <Flex w={"100%"} justify="space-between" align="center">
+          <div>
+            <Title order={1}>Mantine Theme Builder</Title>
+            <Text>
+              Hand-picked shadcn inspired themes that you can copy and paste
+              into your apps build with Mantine.
+            </Text>
+          </div>
+          <div>
+            <ColorSchemeSwitch />
+          </div>
+        </Flex>
+        <Box>{/* ThemeCustomizer can be ignored */}</Box>
 
+        <MantineCards />
       </Stack>
     </Container>
-  )
+  );
 }
