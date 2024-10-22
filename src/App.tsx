5138de352700534abfa108856948d46c81efc929
Abhishek S Lal <abhishekslaltvm@gmail.com>
2024-10-22 23:32:07 +0530

    about page added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {
   Container,
   Divider,
   Group,
+  Modal,
+  ScrollArea,
   Stack,
   Text,
   Title,
@@ -12,23 +14,29 @@ import {
 import MantineCards from "./components/cards/mantine-cards";
 import ColorSchemeSwitch from "./components/color-scheme-switch/color-scheme-switch";
 import ThemeCustomizer from "./components/theme-customizer";
+import { useDisclosure } from "@mantine/hooks";
+import { AboutPage } from "./components/about/about";
 
 export default function ThemesPage() {
- 
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <Container size={"xl"} p={{sm: "md", md: "xl"}} h={"100%"} mt={"sm"}>
+    <Container size={"xl"} p={{ sm: "md", md: "xl" }} h={"100%"} mt={"sm"}>
       <Stack h={"100%"} w={"100%"}>
         <Group w={"100%"} justify="space-between" align="center" wrap="wrap">
           <Box>
             <Title order={1}>Mantine Theme Builder</Title>
             <Text>
-              Shadcn inspired themes for <Anchor target="_blank" href="https://mantine.dev/">Mantine</Anchor> components that you can copy
-              and paste into your apps.
+              Shadcn inspired themes for{" "}
+              <Anchor target="_blank" href="https://mantine.dev/">
+                Mantine
+              </Anchor>{" "}
+              components that you can copy and paste into your apps.
             </Text>
           </Box>
           <Group>
             <Group gap={"0"}>
-              <Button variant="subtle" size="xs" style={{ cursor: "pointer" }}>
+              <Button variant="subtle" size="xs" style={{ cursor: "pointer" }} onClick={open}>
                 About
               </Button>
               <Button variant="subtle" size="xs">
@@ -49,20 +57,33 @@ export default function ThemesPage() {
           <ThemeCustomizer />
         </Box>
 
+        <Modal
+          opened={opened}
+          onClose={close}
+          // title={<Title order={2}>About Mantine Theme Builder</Title>}
+          title={"About the project"}
+          size={"xl"}
+          centered
+          scrollAreaComponent={ScrollArea.Autosize}
+        >
+          <ScrollArea h={"600px"} pe={"xs"}>
+            <AboutPage />
+          </ScrollArea>
+        </Modal>
+
         {/* <ColorSwitcher /> */}
 
         <MantineCards />
 
         <Box mt="auto" w="100%">
           <Divider my="sm" />
-         <Group justify="space-between" align="center">
+          <Group justify="space-between" align="center">
             <Text size="sm">
-              This project is inspired by <Anchor
-                href="https://ui.shadcn.com/themes"
-                target="_blank"
-                rel="noopener noreferrer"
-              >Shadcn Themes.</Anchor>
-              {" "}The source code is available on{" "}
+              This project is inspired by{" "}
+              <Anchor href="https://ui.shadcn.com/themes" target="_blank" rel="noopener noreferrer">
+                Shadcn Themes.
+              </Anchor>{" "}
+              The source code is available on{" "}
               <Anchor
                 href="https://github.com/RubixCube-Innovations/mantine-theme-builder"
                 target="_blank"
@@ -73,18 +94,14 @@ export default function ThemesPage() {
               .
             </Text>
             <Text size="sm" c="dimmed">
-              Made with <span style={{fontSize:"10px"}}>❤️</span> by{" "}
-              <Anchor
-                href="https://rubixcube.tech"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              Made with <span style={{ fontSize: "10px" }}>❤️</span> by{" "}
+              <Anchor href="https://rubixcube.tech" target="_blank" rel="noopener noreferrer">
                 RubixCube Innovations
               </Anchor>
             </Text>
-         </Group>
+          </Group>
         </Box>
       </Stack>
-     </Container>
+    </Container>
   );
 }
---
+++ src/components/about/about.tsx
@@ -0,0 +1,68 @@
+import { Anchor, Box, Divider, List, Stack, Text, Title } from '@mantine/core';
+
+export const AboutPage = () => {
+  return (
+    <Stack>
+      {/* <Title order={2}>About Mantine Theme Builder</Title> */}
+      <Box>
+        <Title order={3}>🤘 Motivation</Title>
+        <Divider my="sm" />
+        <Text>
+          Mantine is an amazing library with a wide range of components, extensive customization options, and a vibrant
+          community. It offers a developer experience unmatched by many React libraries.  However, its default styling can feel 
+          less modern compared to frameworks like shadcn and Radix, which many companies prefer.
+           Setting up colors and styles to match those modern aesthetics can be time-consuming and may hinder projects that need a quick start.
+        </Text>
+        <br />
+        <Text>
+          That's where Mantine Theme Builder comes in. This tool provides pre-configured themes inspired by shadcn,
+          allowing you to easily integrate modern styling into your Mantine project. Simply copy and paste the theme
+          configuration, and you're good to go. In the future, we plan to add more sought-after themes, including the
+          popular glasmorphic theme from Radix.
+        </Text>
+      </Box>
+      <Box mt="md">
+        <Title order={3}>🤔 How to Use</Title>
+        <Divider my="sm" />
+        <List type="ordered">
+          <List.Item>Copy the `theme.ts` file from Mantine Theme Builder and paste it into your project.</List.Item>
+          <List.Item>Import the theme object and pass it to `MantineProvider` in your project.</List.Item>
+          <List.Item>Import the `cssVariableResolver` and pass it to the same component.</List.Item>
+          <List.Item>Copy the `mantine.css` file from the builder and include it in your project.</List.Item>
+        </List>
+        <Text>
+          <br />
+          By following these simple steps, you'll have a modern, shadcn-like theme integrated seamlessly with your
+          Mantine components, saving you time and effort.
+        </Text>
+      </Box>
+      <Box mt={"md"}>
+        <Title order={3}>🧑‍💻 Contributors</Title>
+        <Divider my="sm" />
+        <Text>
+          This project is maintained by{" "}
+          <Anchor href="https://github.com/Abhishek-S-Lal" target="_blank">
+            Abhishek-S-Lal
+          </Anchor>{" "}
+          and{" "}
+          <Anchor href="https://github.com/gopukr" target="_blank">
+            gopukr
+          </Anchor>
+          . Contributions from the community are welcome and appreciated. Please see our CONTRIBUTING.md file for more
+          information on how to get involved.
+        </Text>
+      </Box>
+      <Box mt="md">
+        <Title order={4}>🎗️ Support</Title>
+        <Text>If you find this project useful, plese consider</Text>
+        <List type="unordered">
+          <List.Item icon="⭐️">Starring the repository on GitHub</List.Item>
+          <List.Item icon="🫶">Sharing the project with your friends and colleagues</List.Item>
+          <List.Item icon="👩‍💻">Contributing to the project</List.Item>
+          {/* <List.Item icon="🥤">Buy me a coffee</List.Item> */}
+        </List>
+        <br />
+      </Box>
+    </Stack>
+  );
+};
