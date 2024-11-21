761c9eda019db77e26fa68e0fa5922cb62659003
Abhishek S Lal <abhishekslaltvm@gmail.com>
2024-11-22 00:19:59 +0530

    chore: updated testimonials

--- src/utils/testimonials.ts
+++ src/utils/testimonials.ts
@@ -1,40 +1,101 @@
 export const testimonialItems = [
   //TODO: Replace with original testimonials
   {
-    name: "Mathew",
-    text: "After using this, I cannot imagine going back to the old way of doing things.",
-    handle: "@heymatt_oo",
+    name: "Junior Mantine Shill Charlie",
+    text: "Great work, there's been demand for custom theme solutions for quite a while now. I can see this being heavily used especially if you introduce more themes in the future",
+    handle: "@_.charlie",
+    imageSrc: "https://picsum.photos/100/100.webp?random=4",
+    platform: "discord",
+    url: "https://discord.com/channels/854810300876062770/905183891541856297/1302582803380043837"
+  },
+  {
+    name: "Federico Cappelletti",
+    text: "I literally love you.",
+    handle: "@federicocappelletti",
     imageSrc: "https://picsum.photos/100/100.webp?random=2",
+    platform: "discord",
+    url: "https://discord.com/channels/854810300876062770/905183891541856297/1301611053595234415"
   },
   {
-    name: "Joshua",
-    text: "Perfect for my use case",
-    handle: "@joshua",
-    imageSrc: "https://picsum.photos/100/100.webp?random=3",
+    name: "Orion",
+    text: "Ever since discovering mantine I've felt that this sort of complimentary tool was severely lacking, and would elevate the use of mantine, at least for me, significantly. I'm very excited to see that something is finally here, and of this quality.",
+    handle: "@Shadowfita",
+    imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "github",
+    url: "https://github.com/orgs/mantinedev/discussions/7056#discussioncomment-11329081"
   },
   {
-    name: "Parl Coppa",
-    text: "This is the best thing since sliced bread. I cannot believe I did not think of it myself.",
-    handle: "@coppalipse",
+    name: "Vintage",
+    text: "This is awesome",
+    handle: "@vintage8",
     imageSrc: "https://picsum.photos/100/100.webp?random=1",
     featured: true, // Feature this testimonial
+    platform: "discord",
+    url: "https://discord.com/channels/854810300876062770/905183891541856297/1301627538262720562"
   },
   {
-    name: "Mandy",
-    text: "Excellent product!",
-    handle: "@mandy",
-    imageSrc: "https://picsum.photos/100/100.webp?random=4",
-  },
-  {
-    name: "Alex",
-    text: "Can easily recommend!",
-    handle: "@alex",
+    name: "Brokenwind",
+    text: "Shadcn is dead to me now.",
+    handle: "@broken.wind",
     imageSrc: "https://picsum.photos/100/100.webp?random=5",
+    platform: "discord",
+    url: "https://discord.com/channels/854810300876062770/887408088800448553/1305432194080051231"
   },
   {
-    name: "Sam",
-    text: "I am very happy with the results.",
-    handle: "@sama",
+    name: "Andrej",
+    text: "Thank you for creating such an incredible theme builder for Mantine – it’s a game-changer for my workflow!",
+    handle: "@AndrejNemec",
     imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "github",
+    url: "https://github.com/orgs/mantinedev/discussions/7056#discussioncomment-11116272"
   },
+  {
+    name: "Mordechai Dror",
+    text: "Daaamn)))) I'm more and more in love with mantine.",
+    handle: "@vorant94",
+    imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "discord",
+    url: "https://discord.com/channels/854810300876062770/887408088800448553/1306329638439485572"
+  },
+  {
+    name: "Sajarin M",
+    text: "Nice work! 👍",
+    handle: "@Sajarin-M",
+    imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "github",
+    url: "https://github.com/orgs/mantinedev/discussions/7056#discussioncomment-11118334"
+  },
+  {
+    name: "Ionut-Cristian Florescu",
+    text: "Fantastic work! I'd say this is one of the most valuable Mantine-related projects that have appeared in the community lately.",
+    handle: "@icflorescu",
+    imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "github",
+    url: "https://github.com/orgs/mantinedev/discussions/7056#discussioncomment-11146579"
+  },
+  {
+    name: "Pedro Santos",
+    text: "Amazing project, very useful. congrats team 👏🏻",
+    handle: "@pedrocarlos-ti",
+    imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "github",
+    url: "https://github.com/orgs/mantinedev/discussions/7056#discussioncomment-11213541"
+  },
+  {
+    name: "hjhjdev",
+    text: "Nice work!",
+    handle: "@hjhjdev",
+    imageSrc: "https://picsum.photos/100/100.webp?random=6",
+    platform: "github",
+    url: "https://github.com/orgs/mantinedev/discussions/7056#discussioncomment-11237246"
+  },
+  {
+    name: "Antoine",
+    text: "Looking forward the radix-ui version",
+    handle: "@antoineW",
+    imageSrc: "https://picsum.photos/100/100.webp?random=3",
+    platform: "discord",
+    url: "https://discord.com/channels/854810300876062770/905183891541856297/1301617263216164975"
+  },
+
 ];
