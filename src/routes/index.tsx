05feddc0cd6d6e383ef4340f1c33ba3b8c00e058
Abhishek S Lal <abhishekslaltvm@gmail.com>
2024-11-25 07:38:04 +0530

    fix: fixed deployment errors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { Anchor } from "@mantine/core";
-import * as z from "zod";
 import MantineCards from "../components/custom/theme-example-cards/mantine-cards";
 import PageLayout from "../components/layouts/page-layout";
 
-const indexSearchSchema = z.object({
-  tab: z.string().optional(),
-});
-
 export const Route = createFileRoute("/")({
   component: RouteComponent,
-  validateSearch: indexSearchSchema,
 });
 
 function RouteComponent() {
