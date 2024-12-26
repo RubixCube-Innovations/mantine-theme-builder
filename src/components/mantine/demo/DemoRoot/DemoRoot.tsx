0cd8ac8edbf2789b5e4489a2b3d109c787ed9835
Gopu K Raju <gopukraju02@gmail.com>
2024-12-26 16:06:06 +0530

    fix: demo root interface

--- src/components/mantine/demo/DemoRoot/DemoRoot.tsx
+++ src/components/mantine/demo/DemoRoot/DemoRoot.tsx
@@ -1,8 +1,6 @@
-import cx from 'clsx';
-import classes from './DemoRoot.module.css';
+import cx from "clsx";
+import classes from "./DemoRoot.module.css";
 
-export interface DemoRootProps extends React.ComponentPropsWithoutRef<'div'> {}
-
-export function DemoRoot({ className, ...others }: DemoRootProps) {
+export function DemoRoot({ className, ...others }: React.ComponentPropsWithoutRef<"div">) {
   return <div className={cx(classes.root, className)} {...others} />;
 }
