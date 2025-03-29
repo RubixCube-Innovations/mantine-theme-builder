11751678f96fa6bc8c4619f7f485d74a82afc143
Gopu K Raju <gopukraju02@gmail.com>
2025-03-29 21:46:22 +0530

    feat: add new hero component to blocks

--- src/feature/blocks/lib/index.ts
+++ src/feature/blocks/lib/index.ts
@@ -1,4 +1,6 @@
 export { Hero1 } from "./hero1/hero1";
+export { Hero2 } from "./hero2/hero2";
+
 export { Feature1 } from "./feature1/feature1";
 export { Pricing1 } from "./pricing1/pricing1";
 export { ComingSoon } from "./coming-soon/coming-soon";
