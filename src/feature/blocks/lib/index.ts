68f31ec808cfee8319e023ef68c9f39899b8f4f7
Gopu K Raju <gopukraju02@gmail.com>
2025-04-01 20:32:26 +0530

    feat: extend hero section with new blocks

--- src/feature/blocks/lib/index.ts
+++ src/feature/blocks/lib/index.ts
@@ -1,5 +1,7 @@
 export { Hero1 } from "./hero1/hero1";
 export { Hero2 } from "./hero2/hero2";
+export { Hero3 } from "./hero3/hero3";
+export { Hero4 } from "./hero4/hero4";
 
 export { Feature1 } from "./feature1/feature1";
 export { Pricing1 } from "./pricing1/pricing1";
