8b5e4e3a007afb21b92d6e3aa52e7f4132994897
gopu k r <gopukraju02@gmail.com>
2025-11-29 12:29:07 +0530

    feat: add scroll to top for block pages

--- src/feature/blocks/components/categories/category-page.tsx
+++ src/feature/blocks/components/categories/category-page.tsx
@@ -1,4 +1,8 @@
-import { Container } from "@mantine/core";
+"use client";
+
+import { IconArrowUp } from "@tabler/icons-react";
+import { useWindowScroll } from "@mantine/hooks";
+import { Affix, ActionIcon, Container, Transition } from "@mantine/core";
 import { ComponentCanvas } from "../component-canvas/component-canvas";
 import { Category, UiComponent } from "../../data/types";
 
@@ -8,6 +12,8 @@ interface CategoryPageProps {
 }
 
 const CategoryPage = ({ components }: CategoryPageProps) => {
+  const [scroll, scrollTo] = useWindowScroll();
+
   const canvases = components?.map((component, index) => (
     <ComponentCanvas {...component} key={component.slug} zindex={components.length - index} />
   ));
@@ -15,6 +21,23 @@ const CategoryPage = ({ components }: CategoryPageProps) => {
   return (
     <Container size="xl" pt={"xl"} miw={"100%"} maw={"100%"} px={0}>
       {canvases}
+
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Transition transition="slide-up" mounted={scroll.y > 0}>
+          {(transitionStyles) => (
+            <ActionIcon
+              size="xl"
+              radius="xl"
+              variant="filled"
+              style={transitionStyles}
+              onClick={() => scrollTo({ y: 0 })}
+              aria-label="Scroll to top"
+            >
+              <IconArrowUp size={20} />
+            </ActionIcon>
+          )}
+        </Transition>
+      </Affix>
     </Container>
   );
 };
